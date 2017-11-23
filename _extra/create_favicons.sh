7cec6617e81e5edf350d2f8e030978f918c60972
profesor <profesor@gregoriofer.com>
2017-11-23 18:31:07 +0100

    New icons

---
+++ _extra/create_favicons.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -e
+
+origin=favicon.png
+path=../assets/
+
+echo "favicon.ico"
+convert $origin -define icon:auto-resize=64,48,32,16 $path/favicon.ico
+
+echo "apple-touch-icon-precomposed.png"
+convert $origin -size 180x180 $path/apple-touch-icon-precomposed.png
+
+echo "open-graph-logo.png"
+convert $origin -size 200x200 $path/open-graph-logo.png
